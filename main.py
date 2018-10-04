2e426ec249b75ef34f934e2ed5f880eda18d9ec3
cooler9711 <cooler9711@outlook.com>
2018-10-04 11:34:23 -0600

    now it's faster

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ contents = ""
 with open('../pass.cooler9711@gmail.com.txt') as f:
     for line in f.readlines():
         contents += line
-msg = raw_input("what do you want to email? ")
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login("darthvader9711@gmail.com", contents)
+msg = raw_input("what do you want to email? ")
 server.sendmail("darthvader9711@gmail.com", "4358308794@mms.att.net", msg)
 server.quit()
 print("sent " + msg)
