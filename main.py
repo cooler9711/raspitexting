45b09f16cf6250e492eb55360daead35a4e9526a
cooler9711 <cooler9711@outlook.com>
2018-11-07 19:39:40 -0700

    just made things in general better

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 import smtplib
 
 contents = ""
-with open('../pass.cooler9711@gmail.com.txt') as f:
+with open('../pass.darthvader9711@gmail.com.txt') as f:
     for line in f.readlines():
         contents += line
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login("darthvader9711@gmail.com", contents)
-msg = raw_input("what do you want to email? ")
+msg = raw_input("what is the message that you want to send? ")
 server.sendmail("darthvader9711@gmail.com", "4358308794@mms.att.net", msg)
 server.quit()
-print("sent " + msg)
+print("sent \"" + msg + "\"")
